c468f9062c788c3514df19afc5461abeef3a0bc7
Marwam <marwamprog.js@gmail.com>
2023-03-20 20:47:18 -0300

    Finalizando Projeto Alcool ou Gasolina

--- app/src/main/java/com/cursoandroid/alcoolougasolina/MainActivity.java
+++ app/src/main/java/com/cursoandroid/alcoolougasolina/MainActivity.java
@@ -3,12 +3,72 @@ package com.cursoandroid.alcoolougasolina;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.google.android.material.textfield.TextInputEditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText editPrecoAlcool, editPrecoGasolina;
+    private TextView textResultado;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        editPrecoAlcool = findViewById(R.id.editPrecoAlcool);
+        editPrecoGasolina = findViewById(R.id.editPrecoGasolina);
+        textResultado = findViewById(R.id.textResultado);
+
+    }
+
+    public void calcularPreco(View view) {
+
+        //Recuperar valores digitados
+        String precoAlcool = editPrecoAlcool.getText().toString();
+        String precoGasolina = editPrecoGasolina.getText().toString();
+
+        //Validar os campos digitados
+        Boolean camposValidados =  this.validarCampos(precoAlcool, precoGasolina);
+
+        if( camposValidados ) {
+
+            //Converter String para números
+            Double valorAlcool = Double.parseDouble(precoAlcool);
+            Double valorGasolina = Double.parseDouble(precoGasolina);
+
+            /**
+             * Fazer o cálculo de menor preço
+             * Se (valorAlcool / valorGasolina) >= 0.7 é melhor utilizar gasolina
+             * senão é melhor utiliza álcool
+             */
+            Double resultado = valorAlcool / valorGasolina;
+
+            if(resultado >= 0.7 ) {
+                textResultado.setText("É melhor utilizar gasolina");
+            } else  {
+                textResultado.setText("É melhor utilizar álcool");
+            }
+
+
+        } else  {
+            textResultado.setText("Preencha os preços primeiro!");
+        }
+
+    }
+
+    public Boolean validarCampos(String pAlcool, String pGasolina) {
+        Boolean camposValidados = true;
+
+        if(pAlcool == null || pAlcool.equals("")) {
+            camposValidados = false;
+        } else if (pGasolina == null || pGasolina.equals("")) {
+            camposValidados = false;
+        }
+
+        return camposValidados;
     }
 }
